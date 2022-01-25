081c0c0807ab380700d4e5edebfe574f1b328b64
Mohamed El-Refaie <mohgamal@hayasolutions.com>
2022-01-25 20:31:05 +0200

    ESLint, Prettier & Husky

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    // 'airbnb-base',
+    // 'airbnb-typescript/base',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'implicit-arrow-linebreak': 'off',
+    curly: ['error', 'all'],
+  },
+};
