35b897b737fbaea3f85fe42829b338f7144de843
Mohamed El-Refaie <mohgamal@hayasolutions.com>
2022-01-25 23:31:18 +0200

    Configure Typeorm Migrations and Add Healthchecks

---
+++ src/config/db.config.ts
@@ -0,0 +1,21 @@
+import { registerAs } from '@nestjs/config';
+import { join } from 'path';
+
+export default registerAs('database', () => ({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  applicationName: process.env.APP_NAME,
+  autoLoadEntities: true,
+  synchronize: false,
+  keepConnectionAlive: true,
+  logging: process.env.NODE_ENV === 'development',
+  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  migrations: [join(__dirname, '..', 'migrations/**', '*.{ts,js}')],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+}));
